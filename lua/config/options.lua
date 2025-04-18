2781f22850d0144777f415be05aecefb90c0999b
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2025-04-18 07:51:43 +0200

    sioyek as the pdf viewer

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,4 +2,5 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.g.vimtex_view_method = "zathura_simple"
+-- vim.g.vimtex_view_method = "zathura_simple"
+vim.g.vimtex_view_method = "sioyek"
